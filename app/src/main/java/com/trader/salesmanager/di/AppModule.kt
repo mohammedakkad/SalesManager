6ce4fbd13792731ac634f3aeb15e8fd8f026d1b2
mohammedakkad <mohammed.sofyan.2001@gmail.com>
2026-04-04 21:39:34 +0300

    fix bugs

--- app/src/main/java/com/trader/salesmanager/di/AppModule.kt
+++ app/src/main/java/com/trader/salesmanager/di/AppModule.kt
@@ -4,5 +4,5 @@ import com.trader.salesmanager.data.remote.FirebaseActivationService
 import org.koin.dsl.module
 
 val appModule = module {
-    single { FirebaseActivationService() }
+    single { FirebaseSyncService() }
 }
