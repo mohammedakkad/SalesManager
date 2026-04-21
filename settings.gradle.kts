f89cd27638ce0331c5c43cba55486758a5768d13
mohammedakkad <mohammed.sofyan.2001@gmail.com>
2026-04-21 21:10:05 +0300

    fix bugs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,3 +17,15 @@ dependencyResolutionManagement {
 }
 rootProject.name = "SalesManager"
 include(":app", ":salesmanager", ":admin")
+
+gradle.beforeProject {
+    pluginManager.withPlugin("com.android.library") {
+        extensions.configure<com.android.build.gradle.LibraryExtension> {
+            buildTypes {
+                maybeCreate("staging").apply {
+                    matchingFallbacks += listOf("release")
+                }
+            }
+        }
+    }
+}
